4a436c7f1946807cbd38563dd29bae737e5dc9f9
titho93 <titho93.10@gmail.com>
2023-11-18 11:36:53 +0100

    [add]header and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/js/app.js')
-    <title>home</title>
-</head>
-
-<body>
-
+@section('content')
     <h1>Home</h1>
-
-</body>
-
-</html>
+    <p>Description page</p>
+@endsection
